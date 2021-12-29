a6675757b7d88b57bd35120155e0accd985f5845
Abdurahiman K <laluvga@gmail.com>
2021-12-29 22:06:23 +0530

    initial code

--- main.py
+++ main.py
@@ -1,43 +1,112 @@
 import random
 
 
-def dom_select(computer, player):
-    com_double = [x for x in computer if x[0] == x[1]]
-    pla_double = [x for x in player if x[0] == x[1]]
-    if len(com_double) == 0 and len(pla_double) == 0:
-        return "no_double"
-    elif len(com_double) == 0:
-        return ["computer", max(pla_double)]
-    elif len(pla_double) == 0:
-        return ["player", max(com_double)]
-    else:
-        if max(com_double) > max(pla_double):
-            return ["player", max(com_double)]
-        else:
-            return ["computer", max(pla_double)]
+class Domino:
 
-
-dominoes = [[x, y] for x in range(0, 7) for y in range(x, 7)]
-
-# domino_snake
-while True:
-    random.shuffle(dominoes)
-    player_pieces= dominoes[:7]
-    computer_pieces = dominoes[7:14]
-    stock_pieces = dominoes[14:]
-    status = dom_select(computer_pieces, player_pieces)
-    if status == "no_double":
-        continue
-    else:
-        if status[0] == "computer":
-            player_pieces.remove(status[1])
-            break
-        else:
-            computer_pieces.remove(status[1])
+    def __init__(self):
+        self.dominoes = []
+        while True:
+            for i in range(7):
+                for j in range(i, 7):
+                    self.dominoes.append([i, j])
+            random.shuffle(self.dominoes)
+            self.player = self.dominoes[:7]
+            self.computer = self.dominoes[7:14]
+            self.stock = self.dominoes[14:]
+            self.snake = [[6, 6]]
+            self.next_move = ""
+            self.count = 0
+            self.domino = 0
+            for i in range(5, -1, -1):
+                if self.snake[0] in self.player:
+                    self.player.remove(self.snake[0])
+                    self.next_move = "computer"
+                    break
+                if self.snake[0] in self.computer:
+                    self.computer.remove(self.snake[0])
+                    self.next_move = "player"
+                    break
+                self.snake = [[i, i]]
+            else:
+                continue
             break
 
-print(f"Stock pieces: {stock_pieces}")
-print(f"Computer pieces: {computer_pieces}")
-print(f"Player pieces: {player_pieces}")
-print(f"Domino snake: {[status[1]]}")
-print(f"Status: {status[0]}")
+    def status(self):
+        if len(self.player) == 0:
+            return "Status: The game is over. You won!"
+        elif len(self.computer) == 0:
+            return "Status: The game is over. The computer won!"
+        else:
+            if self.snake[0][0] == self.snake[-1][1]:
+                for i in self.snake:
+                    for j in i:
+                        if self.snake[0][0] == j:
+                            self.count += 1
+            return "Status: The game is over. It's a draw!" if self.count == 8 else None
+
+    def player_move(self):
+        while True:
+            try:
+                self.domino = int(input("\nStatus: It's your turn to make a move. Enter your command."))
+            except ValueError:
+                print("\nInvalid input. Please try again.")
+                continue
+            if abs(self.domino) > len(self.player):
+                print("\nInvalid input. Please try again.")
+                continue
+            break
+        if self.domino == 0:
+            choice = random.choice(self.stock)
+            self.stock.remove(choice)
+            self.player.append(choice)
+        elif self.domino < 0:
+            self.snake.insert(0, self.player[abs(self.domino) - 1])
+            self.player.remove(self.player[abs(self.domino) - 1])
+        else:
+            self.snake.append(self.player[self.domino - 1])
+            self.player.remove(self.player[abs(self.domino) - 1])
+
+    def computer_move(self):
+        any_key = input("\nStatus: Computer is about to make a move. Press Enter to continue...")
+        self.domino = random.choice([i for i in range(-len(self.computer), len(self.computer))])
+        if self.domino == 0:
+            choice = random.choice(self.stock)
+            self.stock.remove(choice)
+            self.computer.append(choice)
+        elif self.domino < 0:
+            self.snake.insert(0, self.computer[abs(self.domino) - 1])
+            self.computer.remove(self.computer[abs(self.domino) - 1])
+        else:
+            self.snake.append(self.computer[self.domino - 1])
+            self.computer.remove(self.computer[abs(self.domino) - 1])
+
+    def play(self):
+        while True:
+            print("=" * 70)
+            print(f"Stock size: {len(self.stock)}")
+            print(f"Computer pieces: {len(self.computer)}\n")
+            if len(self.snake) <= 6:
+                for i in self.snake:
+                    print(i, end="")
+            else:
+                print(f"{self.snake[0]}{self.snake[1]}{self.snake[2]}...", end="")
+                print(f"{self.snake[-3]}{self.snake[-2]}{self.snake[-1]}")
+            print(end='\n')
+            print(f"\nYour pieces:")
+            for i in range(len(self.player)):
+                print(f"{i + 1}:{self.player[i]}")
+            if self.status() is None:
+                if self.next_move == "player":
+                    self.player_move()
+                    self.next_move = "computer"
+                else:
+                    self.computer_move()
+                    self.next_move = "player"
+                continue
+            else:
+                print(self.status())
+                break
+
+
+dom = Domino()
+dom.play()
--- test.py
+++
@@ -1,7 +0,0 @@
-list_1 = [[0, 0], [2, 2], [5, 6], [6, 3], [7, 1]]
-list_2 = []
-if max(list_1) > max(list_2):
-    print(["list_1", max(list_1)])
-else:
-    print(["list_2", max(list_2)])
-
