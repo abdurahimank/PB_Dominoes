fccd4a4d1bdf89dbd66e54a4cba681cfad2b8959
Abdurahiman K <laluvga@gmail.com>
2021-12-31 10:26:18 +0530

    Final code of first release

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import random
 
 
 class Domino:
-
     def __init__(self):
         self.dominoes = []
+        self.next_move = ""
+        self.count = 0
+        self.domino = 0
         while True:
             for i in range(7):
                 for j in range(i, 7):
@@ -14,9 +16,6 @@ class Domino:
             self.computer = self.dominoes[7:14]
             self.stock = self.dominoes[14:]
             self.snake = [[6, 6]]
-            self.next_move = ""
-            self.count = 0
-            self.domino = 0
             for i in range(5, -1, -1):
                 if self.snake[0] in self.player:
                     self.player.remove(self.snake[0])
@@ -38,11 +37,33 @@ class Domino:
             return "Status: The game is over. The computer won!"
         else:
             if self.snake[0][0] == self.snake[-1][1]:
+                self.count = 0
                 for i in self.snake:
                     for j in i:
                         if self.snake[0][0] == j:
                             self.count += 1
             return "Status: The game is over. It's a draw!" if self.count == 8 else None
+
+    def legal_move(self, turn):
+        if self.domino == 0:
+            return "not good"
+        slide = turn[abs(self.domino) - 1]
+        if self.domino < 0:
+            if slide[1] == self.snake[0][0]:
+                return "good"
+            elif slide[0] == self.snake[0][0]:
+                turn[abs(self.domino) - 1] = [slide[1], slide[0]]
+                return "good"
+            else:
+                return "not good"
+        else:
+            if slide[0] == self.snake[-1][1]:
+                return "good"
+            elif slide[1] == self.snake[-1][1]:
+                turn[abs(self.domino) - 1] = [slide[1], slide[0]]
+                return "good"
+            else:
+                return "not good"
 
     def player_move(self):
         while True:
@@ -54,11 +75,15 @@ class Domino:
             if abs(self.domino) > len(self.player):
                 print("\nInvalid input. Please try again.")
                 continue
+            if self.legal_move(self.player) == "not good" and self.domino != 0:
+                print("Illegal move. Please try again.")
+                continue
             break
         if self.domino == 0:
-            choice = random.choice(self.stock)
-            self.stock.remove(choice)
-            self.player.append(choice)
+            if len(self.stock) != 0:
+                choice = random.choice(self.stock)
+                self.stock.remove(choice)
+                self.player.append(choice)
         elif self.domino < 0:
             self.snake.insert(0, self.player[abs(self.domino) - 1])
             self.player.remove(self.player[abs(self.domino) - 1])
@@ -66,19 +91,68 @@ class Domino:
             self.snake.append(self.player[self.domino - 1])
             self.player.remove(self.player[abs(self.domino) - 1])
 
+    def domino_score(self, snake, computer):
+        num_score = {i: 0 for i in range(7)}
+        snake_comp = [j for i in (snake + computer) for j in i]
+        for i in snake_comp:
+            for k in range(7):
+                if k == i:
+                    num_score[k] += 1
+        # print(num_score)  # for testing
+        # To find score for dominoes in computer inventory
+        domino_rank = []
+        domino_rank = sorted([f"{num_score[i[0]] + num_score[i[1]]}{i[0]}{i[1]}" for i in computer], reverse=True)
+        return [[int(i[1]), int(i[2])] if len(i) == 3 else [int(i[2]), int(i[3])] for i in domino_rank]
+
     def computer_move(self):
         any_key = input("\nStatus: Computer is about to make a move. Press Enter to continue...")
-        self.domino = random.choice([i for i in range(-len(self.computer), len(self.computer))])
-        if self.domino == 0:
-            choice = random.choice(self.stock)
-            self.stock.remove(choice)
-            self.computer.append(choice)
-        elif self.domino < 0:
-            self.snake.insert(0, self.computer[abs(self.domino) - 1])
-            self.computer.remove(self.computer[abs(self.domino) - 1])
+        computer_list = [i for j in self.computer for i in j]
+        # Below code is used to directly go to skipping if there are no legal moves
+        if self.snake[0][0] not in computer_list and self.snake[-1][1] not in computer_list:
+            if len(self.stock) != 0:
+                choice = random.choice(self.stock)
+                self.stock.remove(choice)
+                self.computer.append(choice)
+        # Method - 1
+        # The domino is found by highest score method
         else:
-            self.snake.append(self.computer[self.domino - 1])
-            self.computer.remove(self.computer[abs(self.domino) - 1])
+            domino_ranked = self.domino_score(self.snake, self.computer)
+            for i in domino_ranked:
+                if self.snake[0][0] == i[0]:
+                    self.snake.insert(0, [i[1], i[0]])
+                elif self.snake[0][0] == i[1]:
+                    self.snake.insert(0, i)
+                elif self.snake[-1][1] == i[0]:
+                    self.snake.append(i)
+                elif self.snake[-1][1] == i[1]:
+                    self.snake.append([i[1], i[0]])
+                else:
+                    continue
+                self.computer.remove(i)
+                break
+
+        '''
+        # Method - 2
+        # The domino is found by random choice subject to other conditions
+        else:
+            legal_list = []
+            for i in self.computer:
+                if self.snake[0][0] == i[0] or self.snake[0][0] == i[1]:
+                    legal_list.append(i)
+                    continue
+                if self.snake[-1][1] == i[0] or self.snake[-1][1] == i[1]:
+                    legal_list.append(i)
+                    continue
+            choice = random.choice(legal_list)
+            self.computer.remove(choice)
+            if choice[0] == self.snake[0][0]:
+                self.snake.insert(0, [choice[1], choice[0]])
+            elif choice[1] == self.snake[0][0]:
+                self.snake.insert(0, choice)
+            elif choice[0] == self.snake[-1][1]:
+                self.snake.append(choice)
+            else:
+                self.snake.append([choice[1], choice[0]])'''
 
     def play(self):
         while True:
